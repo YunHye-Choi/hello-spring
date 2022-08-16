67a5bcb5ea5c87b1bf3885209a47ac14cc860a11
yunhye-choi <anffl98@gmail.com>
2022-08-16 15:42:06 +0900

    강의 수강 완료

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,10 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
@@ -21,6 +23,7 @@ public class MemberService {
      * 회원 가입
      *  - 같은 이름을 가진 회원 존재 불가
      * */
+
     public Long join(Member member){
         validateDuplicateMember(member); // 중복 회원 검증
         memberRepository.save(member);
